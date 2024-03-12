92b852b6dc1a39828a9c2a84d3154a24c0b4f6a1
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-03-11 22:31:54 -0700

    added first functions in js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const board = document.getElementById('gameBoard');
 
+let snake = [{ x: 10, y: 10 }];
+// draw gameboard, snake, food
+function draw() {
+  board.innerHTML = '';
+  drawSnake();
+}
+
+function drawSnake() {
+  snake.forEach(segment => {
+    const snakeElement = createGameElement('div', 'snake');
+    setPosition(snakeElement, segment);
+  });
+}
+
+function createGameElement(tag, className) {
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+}
+
+function setPosition(element, position) {
+  element.style.gridColumn = position.x;
+  element.style.gridRow = position.y;
+}
