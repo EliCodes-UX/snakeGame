1d0c425b1c9e092a3db29daef1acf3e4853ede80
EliCodes-UX <141448128+EliCodes-UX@users.noreply.github.com>
2024-03-11 22:52:12 -0700

    adde d array to snake head to increase the snake

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const board = document.getElementById('gameBoard');
 
+const gridSize = 20;
 let snake = [{ x: 10, y: 10 }];
+let food = generateFood();
+let direction = 'right';
 // draw gameboard, snake, food
 function draw() {
   board.innerHTML = '';
   drawSnake();
+  drawFood();
 }
 
 function drawSnake() {
   snake.forEach(segment => {
     const snakeElement = createGameElement('div', 'snake');
     setPosition(snakeElement, segment);
+    board.appendChild(snakeElement);
   });
 }
 
@@ -23,4 +28,34 @@ function createGameElement(tag, className) {
 function setPosition(element, position) {
   element.style.gridColumn = position.x;
   element.style.gridRow = position.y;
+}
+
+function drawFood() {
+  const foodElement = createGameElement('div', 'food');
+  setPosition(foodElement, food);
+  board.appendChild(foodElement);
+}
+function generateFood() {
+  const x = Math.floor(Math.random() * gridSize) + 1;
+  const y = Math.floor(Math.random() * gridSize) + 1;
+  return { x, y };
+}
+
+function move() {
+  const head = { ...snake[0] };
+  switch (direction) {
+    case 'up':
+      head.y--;
+      break;
+    case 'down':
+      head.y++;
+      break;
+    case 'right':
+      head.x++;
+      break;
+    case 'left':
+      head.x--;
+      break;
+  }
+  snake.unshift(head);
 }
